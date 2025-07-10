6fab7dd0eedc1e637cf27c8ee1a235491a6458e3
shubham karad <shubham.karad12@gmail.com>
2025-07-10 23:18:31 +0530

    counter updated

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [number,setNumber]=useState(0);
 
-  const increment = () => setCount(prev => prev + 1);
-  const decrement = () => setCount(prev => prev - 1);
-  const reset = () => setCount(0);
+  const incrementNum=()=>setNumber(number+1);
+  const decreamentNum=()=>{
+    if(number>0){
+       setNumber(number-1);
+    }
+  }
+ 
 
   return (
     <div className="App">
-      <h1>React Counter</h1>
-      <h2>{count}</h2>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-      <button onClick={reset}>Reset</button>
+      <h1>React counter</h1>
+      <div class='counterClass'>
+        
+        <h3>{number}</h3>
+        <button onClick={incrementNum}>+1</button>
+        <button onClick={decreamentNum}>-1</button>
+      </div>
     </div>
   );
 }
